7364d4e30c04f473f27b2cc14be59f5f44c74143
Chris <chris@livefyre.com>
2012-04-25 01:34:19 -0700

    adding optional display_name to token generator

--- livefyre/client/token.py
+++ livefyre/client/token.py
@@ -2,11 +2,12 @@ import jwt, time
 
 class LFAuthToken(object):
     """Class to create tokens for auth with Livefyre services."""
-    def __init__(self, user, domain, key, duration=86400):
+    def __init__(self, user, domain, key, display_name=None, duration=86400):
         self.domain = domain
         self.duration = duration
         self.key = key
         self.user = user
+        self.display_name = display_name
     
     def __str__(self):
         """Return the generated token string."""
@@ -18,4 +19,6 @@ class LFAuthToken(object):
         token = dict(expires=self.duration + time.time(),
                      user_id=self.user,
                      domain=self.domain)
+        if self.display_name:
+            token['display_name'] = self.display_name
         return jwt.encode(token, self.key)
